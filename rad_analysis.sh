9c87a0e571014e5d9b38515e88ec19bf68409206
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-06-18 15:39:29 +0300

    add: script for getting rad-source data

---
+++ rad_analysis.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BASE_PATH="/Users/abakhtin22/Documents/StaticTools/SARbenchmarks"
+PROJECTS=$(<project_list.txt)
+
+for PROJECT in $PROJECTS; do
+    PROJECT_PATH="${BASE_PATH}/${PROJECT}"
+    echo $PROJECT
+    curl --request POST \
+        --url http://localhost:8080/ \
+        --header 'content-type: application/json' \
+        --data "{
+      \"pathToMsRoots\": [\"${PROJECT_PATH}\"]
+    }" > "${BASE_PATH}/${PROJECT}-output/rad-source-raw.json"
+done
+
