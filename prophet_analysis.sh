6211669a25df34793cfd51ec63c34c9099b6bdfb
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-06-18 15:32:49 +0300

    add: script for getting prophet data

---
+++ prophet_analysis.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BASE_PATH="/Users/abakhtin22/Documents/StaticTools/SARbenchmarks"
+PROJECTS=$(<project_list.txt)
+
+for PROJECT in $PROJECTS; do
+    PROJECT_PATH="${BASE_PATH}/${PROJECT}"
+    echo $PROJECT
+    curl --request POST \
+        --url http://localhost:8081/ \
+        --header 'content-type: application/json' \
+        --data "{
+        \"repositories\": [{\"path\": \"${PROJECT_PATH}\", \"isMonolith\": false, \"local\": true}], \"systemName\": \"${PROJECT}\"
+    }" > "${BASE_PATH}/${PROJECT}-output/prophet-raw.json"
+done
+
