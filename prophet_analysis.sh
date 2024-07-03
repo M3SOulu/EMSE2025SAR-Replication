ac6f10e4b94715e04f685dd2ca263d3ef1e5f463
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-07-03 12:28:54 +0300

    add: automatically process prohet output

--- prophet_analysis.sh
+++ prophet_analysis.sh
@@ -5,12 +5,15 @@ PROJECTS=$(<spring_project_list.txt)
 for PROJECT in $PROJECTS; do
     PROJECT_PATH="${PWD}/${PROJECT}"
     OUTPUT_PATH=${PROJECT_PATH}-output
+    RAW_PATH=${OUTPUT_PATH}/prophet-raw.json
+    PROCESSED_PATH=${OUTPUT_PATH}/prophet-processed.mmd
     echo "Analyzing project $PROJECT"
     curl --request POST \
         --url http://localhost:8081/ \
         --header 'content-type: application/json' \
         --data "{
         \"repositories\": [{\"path\": \"${PROJECT_PATH}\", \"isMonolith\": false, \"local\": true}], \"systemName\": \"${PROJECT}\"
-    }" > "$OUTPUT_PATH/prophet-raw.json"
+    }" > "$RAW_PATH"
+    jq -r .global.communication $RAW_PATH > $PROCESSED_PATH
 done
 
