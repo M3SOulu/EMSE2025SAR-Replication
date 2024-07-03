72e26fee0f63df7dbf1c5decd5ac207ff772ddff
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-07-03 11:33:42 +0300

    add: generate output dir

--- prophet_analysis.sh
+++ prophet_analysis.sh
@@ -4,12 +4,13 @@ PROJECTS=$(<spring_project_list.txt)
 
 for PROJECT in $PROJECTS; do
     PROJECT_PATH="${PWD}/${PROJECT}"
+    OUTPUT_PATH=${PROJECT_PATH}-output
     echo "Analyzing project $PROJECT"
     curl --request POST \
         --url http://localhost:8081/ \
         --header 'content-type: application/json' \
         --data "{
         \"repositories\": [{\"path\": \"${PROJECT_PATH}\", \"isMonolith\": false, \"local\": true}], \"systemName\": \"${PROJECT}\"
-    }" > "$${PROJECT_PATH}-output/prophet-raw.json"
+    }" > "$OUTPUT_PATH/prophet-raw.json"
 done
 
