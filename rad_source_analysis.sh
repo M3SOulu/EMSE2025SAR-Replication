950a5af584d5551a4a0ed1388010567be10f0a0f
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-07-03 11:16:10 +0300

    add: create output dir

--- rad_source_analysis.sh
+++ rad_source_analysis.sh
@@ -4,12 +4,14 @@ PROJECTS=$(<spring_project_list.txt)
 
 for PROJECT in $PROJECTS; do
     PROJECT_PATH="${PWD}/${PROJECT}"
+    OUTPUT_PATH=${PROJECT_PATH}-output
+    mkdir -p $OUTPUT_PATH
     echo "Analyzing projects $PROJECT"
     curl --request POST \
         --url http://localhost:8080/ \
         --header 'content-type: application/json' \
         --data "{
       \"pathToMsRoots\": [\"${PROJECT_PATH}\"]
-    }" > "${PROJECT_PATH}-output/rad-source-raw.json"
+    }" > "$OUTPUT_PATH/rad-source-raw.json"
 done
 
