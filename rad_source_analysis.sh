16f170fab1da5ea6def67ab321ade4ae86e03390
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-07-07 19:08:59 +0300

    add: pass paths to all services in the request

--- rad_source_analysis.sh
+++ rad_source_analysis.sh
@@ -3,15 +3,22 @@
 PROJECTS=$(<spring_project_list.txt)
 
 for PROJECT in $PROJECTS; do
+    echo "Analyzing projects $PROJECT"
+
+    SERVICES=()
     PROJECT_PATH="${PWD}/${PROJECT}"
+    SERVICE_LIST=$PROJECT_PATH-service-list.txt
+    while IFS= read -r line; do
+        SERVICES+=("$PROJECT_PATH/$line")
+    done < "$SERVICE_LIST"
+    json_array=$(printf '%s\n' "${SERVICES[@]}" | jq -R . | jq -s .)
+    json_payload=$(jq -n --argjson paths "$json_array" '{"pathToMsRoots": $paths}')
+
     OUTPUT_PATH=${PROJECT_PATH}-output
     mkdir -p $OUTPUT_PATH
-    echo "Analyzing projects $PROJECT"
     curl --request POST \
         --url http://localhost:8080/ \
         --header 'content-type: application/json' \
-        --data "{
-      \"pathToMsRoots\": [\"${PROJECT_PATH}\"]
-    }" > "$OUTPUT_PATH/rad-source-raw.json"
+        --data "$json_payload" > "$OUTPUT_PATH/rad-source-raw.json"
 done
 
