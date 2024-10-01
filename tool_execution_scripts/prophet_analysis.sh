1b21c51482d9647820d8fe896dfbd858aa662bce
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-10-01 18:26:26 +0300

    fix: prophet script

--- tool_execution_scripts/prophet_analysis.sh
+++ tool_execution_scripts/prophet_analysis.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-PROJECTS=$(<spring_project_list.txt)
+PROJECTS=$(<tool_configs/spring_project_list.txt)
 
 for PROJECT in $PROJECTS; do
-    PROJECT_PATH="${PWD}/${PROJECT}"
-    OUTPUT_PATH=${PROJECT_PATH}-output
-    RAW_PATH=${OUTPUT_PATH}/prophet-raw.json
-    PROCESSED_PATH=${OUTPUT_PATH}/prophet-processed.mmd
-    SERVICES_PATH=${OUTPUT_PATH}/prophet-services.txt
+    PROJECT_PATH="analyzed_applications/${PROJECT}"
+    OUTPUT_PATH="${PWD}/raw_results/Prophet"
+    RAW_PATH="${OUTPUT_PATH}/${PROPHET}-prophet-raw.json"
+    GRAPH_PATH="${OUTPUT_PATH}/${PROPHET}-prophet-graph.mmd"
+    mkdir -p $OUTPUT_PATH
+
     echo "Analyzing project $PROJECT"
+
     curl --request POST \
         --url http://localhost:8081/ \
         --header 'content-type: application/json' \
         --data "{
         \"repositories\": [{\"path\": \"${PROJECT_PATH}\", \"isMonolith\": false, \"local\": true}], \"systemName\": \"${PROJECT}\"
     }" > "$RAW_PATH"
-    jq -r .global.communication $RAW_PATH > $PROCESSED_PATH
-    jq -r '.ms[] | "\(.name)"' $RAW_PATH > $SERVICES_PATH
+
+    jq -r .global.communication $RAW_PATH > $GRAPH_PATH
 done
 
