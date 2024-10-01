bee699c9f07d53e7f494fa9c8cb249bcee904d39
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-10-01 17:17:58 +0300

    fix: rad script

--- tool_execution_scripts/rad_analysis.sh
+++ tool_execution_scripts/rad_analysis.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-PROJECT_FILE="compiled_spring_project_list.txt"
+PROJECT_FILE="tool_configs/compiled_spring_project_list.txt"
 
 while IFS=, read -r PROJECT ORG_PATH; do
-        PATH_HERE=${PWD}/$PROJECT
-        OUTPUT_PATH="${PATH_HERE}-output/rad-raw"
+        PROJECT_PATH="${PWD}/analyzed_applications/$PROJECT"
+        OUTPUT_PATH="${PWD}/raw_results/RAD"
         mkdir -p $OUTPUT_PATH
+
         echo "Analyzing project $PROJECT"
+
         curl --request POST \
             --url http://localhost:8080/ \
             --header 'content-type: application/json' \
             --data "{
-              \"pathToCompiledMicroservices\": \"${PATH_HERE}\", \"organizationPath\":\"${ORG_PATH}\", \"outputPath\":\"${OUTPUT_PATH}/rad-graph.json\"
-          }" > "$OUTPUT_PATH/rad-response.json"
+              \"pathToCompiledMicroservices\": \"${PROJECT_PATH}\", \"organizationPath\":\"${ORG_PATH}\", \"outputPath\":\"${OUTPUT_PATH}/${PROJECT}-rad-graph.json\"
+          }" > "$OUTPUT_PATH/${PROJECT}-rad-response.json"
 done < "$PROJECT_FILE"
 
