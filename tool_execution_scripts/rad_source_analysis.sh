0d94fcf28058abef9ed0e45e4ada2549d878ae01
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-11-05 13:41:03 +0200

    fix: rad_source script

--- tool_execution_scripts/rad_source_analysis.sh
+++ tool_execution_scripts/rad_source_analysis.sh
@@ -7,14 +7,14 @@ for PROJECT in $PROJECTS; do
 
     SERVICES=()
     PROJECT_PATH="${PWD}/analyzed_applications/${PROJECT}"
-    SERVICE_LIST="tool_configs/rad_source/${PROJECT}-service-list.txt
+    SERVICE_LIST="${PWD}/tool_configs/rad-source/${PROJECT}-service-list.txt"
     while IFS= read -r line; do
         SERVICES+=("$PROJECT_PATH/$line")
     done < "$SERVICE_LIST"
     json_array=$(printf '%s\n' "${SERVICES[@]}" | jq -R . | jq -s .)
     json_payload=$(jq -n --argjson paths "$json_array" '{"pathToMsRoots": $paths}')
 
-    OUTPUT_PATH="raw_results/RAD-source"
+    OUTPUT_PATH="${PWD}/raw_results/RAD-source"
     mkdir -p $OUTPUT_PATH
     curl --request POST \
         --url http://localhost:8080/ \
